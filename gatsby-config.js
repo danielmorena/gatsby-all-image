42ae616fca2a87f5632b71128e534c4a9dc88d94
Sebastian Stüber <sebastian.stueber@rwth-aachen.de>
2018-01-23 16:45:50 +0100

    all images in data/ directory on index displayed

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,16 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`, // you can use multiple source-filesystem instances
+        path: `${__dirname}/data/`,
+      },
+    },
+  ],
 };
